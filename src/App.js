685ceced94419527dc37c9b491d144102e8584c9
Nick Guimarais <Nick.guimarais@gmail.com>
2021-03-29 22:21:24 -0700

    deploying

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
     return (
         <Router>
             <Switch>
-              <Route path="/">
+              <Route exact path="/">
                <Main />            
               </Route>
             </Switch>
