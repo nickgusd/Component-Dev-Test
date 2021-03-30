00ccb68ce165ac83fe0b67338f7cb28cdc46f65f
Nick Guimarais <Nick.guimarais@gmail.com>
2021-03-29 22:30:06 -0700

    updating redirect back home

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default function App() {
               <Route path="/">
                <Main />            
               </Route>
+              <Redirect from="*" to="/" />
             </Switch>
           </Router>
     )
